dc7fc03479f46976fe60d2e25a68a9089045f70f
muho <hi@muho.dev>
2022-03-19 16:08:20 +0300

    Initial web project by create-next-app package

---
+++ api/README.md
@@ -0,0 +1,1 @@
+# Fybe Api
---
+++ web/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fybe</title>
+        <meta
+          name="description"
+          content="Connect, share, and build own communities"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Home Page</main>
+    </div>
+  );
+}
