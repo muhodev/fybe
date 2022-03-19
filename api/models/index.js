5a937b426f21fa2a5ab0e6a41db2f88d2884bd33
muho <hi@muho.dev>
2022-03-19 18:47:24 +0300

    Rename files and export in models index.js

--- api/models/index.js
+++ api/models/index.js
@@ -1,1 +1,5 @@
 export * from "./users.model.js";
+export * from "./categories.model.js";
+export * from "./communities.model.js";
+export * from "./posts.model.js";
+export * from "./tags.model.js";
